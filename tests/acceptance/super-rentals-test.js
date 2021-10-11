67020977232a67caf104a4c75900f0c42f6e734c
Waihon Yew <yewwaihon@gmail.com>
2021-10-12 00:11:00 +0800

    Test visiting /getting-in-touch path

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -28,4 +28,16 @@ module('Acceptance | super rentals', function (hooks) {
 
     assert.equal(currentURL(), '/getting-in-touch');
   });
+
+  test('visiting /getting-in-touch', async function (assert) {
+    await visit('/getting-in-touch');
+
+    assert.equal(currentURL(), '/getting-in-touch');
+    assert.dom('h2').hasText('Contact Us');
+
+    assert.dom('.jumbo a.button').hasText('About');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/about');
+  })
 });
