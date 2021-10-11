c55c92b7ef5ce722389c121e7b079d5a0ca0a1e3
Waihon Yew <yewwaihon@gmail.com>
2021-10-12 00:04:17 +0800

    Test visiting / path

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -1,13 +1,19 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { click, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | super rentals', function (hooks) {
   setupApplicationTest(hooks);
 
-  test('visiting /super-rentals', async function (assert) {
-    await visit('/super-rentals');
+  test('visiting /', async function (assert) {
+    await visit('/');
 
-    assert.equal(currentURL(), '/super-rentals');
+    assert.equal(currentURL(), '/');
+    assert.dom('h2').hasText('Welcome to Super Rentals!');
+
+    assert.dom('.jumbo a.button').hasText('About Us');
+    await click('.jumbo a.button');
+
+    assert.equal(currentURL(), '/about');
   });
 });
