549ba81fb8512ff2e53d6fd85cd87e595e5210a0
Waihon Yew <yewwaihon@gmail.com>
2021-10-15 15:15:42 +0800

    Initialize isLarge instance variable in the constructor

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -1,3 +1,8 @@
 import Component from '@glimmer/component';
 
-export default class RentalImageComponent extends Component {}
+export default class RentalImageComponent extends Component {
+  constructor(...args) {
+    super(...args);
+    this.isLarge = false;
+  }
+}
