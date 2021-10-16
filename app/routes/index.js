8c4b470de905800de0077669e101b216048bb75c
Waihon Yew <yewwaihon@gmail.com>
2021-10-16 22:08:46 +0800

    Fetch remote data from the model hook

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,21 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
-  // This async method is akso known as a model hook. 
+  // This async method is akso known as a model hook.
   // It is responsible for fetching and preparing any data that we need for our route.
   // Ember will automatically call this hook when entering a route.
   // The object returned from this hook is known as the model for the rouote.
   async model() {
-    return {
-      title: 'Grand Old Mansion',
-      owner: 'Veruca Salt',
-      city: 'San Francisco',
-      location: {
-        lat: 37.7749,
-        lng: -122.4194,
-      },
-      category: 'Estate',
-      type: 'Standalone',
-      bedrooms: 15,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-      description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
-    };
+    // Use the browser's Fetch API to request JSON data from our
+    // server's API at public/api/rentals.json.
+    // The Fetch API returns a response object asynchronously.
+    let response = await fetch('/api/rentals.json');
+
+    // We knew the server sent the data using the JSON format,
+    // so we can use the json() method to parse the response data accordingly.
+    // Parsing the reponse data is also an asynchronous operation.
+    let parsed = await response.json();
+
+    return parsed;
   }
 }
