50da8995d08b8d65d5ad7f2aa50e859b11e7ef24
Waihon Yew <yewwaihon@gmail.com>
2021-10-16 15:59:57 +0800

    Implement the Map component template and class

--- app/components/map.js
+++ app/components/map.js
@@ -1,3 +1,10 @@
 import Component from '@glimmer/component';
+import ENV from 'super-rentals/config/environment'
 
-export default class MapComponent extends Component {}
+export default class MapComponent extends Component {
+  get token() {
+    // URL-encode the token, just in case it contains any
+    // special characters that are not URL-safe.
+    return encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
+  }
+}
