e14d80fc71344a0e70e46de50f8d1c0c67c2a025
Waihon Yew <yewwaihon@gmail.com>
2021-10-17 17:32:47 +0800

    Add tests for new Rental::Detailed component and share common setup code between them

--- tests/integration/components/rental/detailed-test.js
+++ tests/integration/components/rental/detailed-test.js
@@ -6,21 +6,51 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | rental/detailed', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  // Use beforeEach hook to share some boilerplate code, which allow us to have
+  // two tests that each focus on a different, single aspect of the component.
+  hooks.beforeEach(function () {
+    this.setProperties({
+      rental: {
+        id: 'grand-old-mansion',
+        title: 'Grand Old Mansion',
+        owner: 'Veruca Salt',
+        city: 'San Francisco',
+        location: {
+          lat: 37.7749,
+          lng: -122.4194,
+        },
+        category: 'Estate',
+        type: 'Standalone',
+        bedrooms: 15,
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+        description:
+          'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
+      },
+    });
+  });
 
-    await render(hbs`<Rental::Detailed />`);
+  test('it renders a header with a share button', async function (assert) {
+    await render(hbs`<Rental::Detailed @rental={{this.rental}} />`);
 
-    assert.dom(this.element).hasText('');
+    assert.dom('.jumbo').exists();
+    assert.dom('.jumbo h2').containsText('Grand Old Mansion');
+    assert
+      .dom('.jumbo p')
+      .containsText('a nice place to say near San Francisco');
+    assert.dom('.jumbo a.button').containsText('Share on Twitter');
+  });
 
-    // Template block usage:
-    await render(hbs`
-      <Rental::Detailed>
-        template block text
-      </Rental::Detailed>
-    `);
+  test('it renders detailed information about a rental property', async function (assert) {
+    await render(hbs`<Rental::Detailed @rental={{this.rental}} />`);
 
-    assert.dom(this.element).hasText('template block text');
+    assert.dom('article').hasClass('rental');
+    assert.dom('article h3').containsText('About Grand Old Mansion');
+    assert.dom('article .detail.owner').containsText('Veruca Salt');
+    assert.dom('article .detail.type').containsText('Standalone - Estate');
+    assert.dom('article .detail.location').containsText('San Francisco');
+    assert.dom('article .detail.bedrooms').containsText('15');
+    assert.dom('article .image').exists();
+    assert.dom('article .map').exists();
   });
 });
