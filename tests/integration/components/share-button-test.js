9e76b6a7fcd403ffc25ae4f37708afe91d5f6db7
Waihon Yew <yewwaihon@gmail.com>
2021-10-17 22:02:05 +0800

    Test the key functionalities of the ShareButton component individually

--- tests/integration/components/share-button-test.js
+++ tests/integration/components/share-button-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import Service from '@ember/service';
-import { render } from '@ember/test-helpers';
+import { find, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 class MockRouterService extends Service {
@@ -29,6 +29,12 @@ module('Integration | Component | share-button', function (hooks) {
     // which acceptance tests provide end-to-end coverage that ensures
     // that these components do inddeed work well together.
     this.owner.register('service:router', MockRouterService);
+
+    this.tweetParam = (param) => {
+      let link = find('a');
+      let url = new URL(link.href);
+      return url.searchParams.get(param);
+    };
   });
 
   test('basic usage', async function (assert) {
@@ -38,14 +44,63 @@ module('Integration | Component | share-button', function (hooks) {
       .dom('a')
       .hasAttribute('target', '_blank')
       .hasAttribute('rel', 'external nofollow noopener noreferrer')
-      .hasAttribute(
-        'href',
-        `https://twitter.com/intent/tweet?url=${encodeURIComponent(
-          new URL('/foo/bar?baz=true#some-section', window.location.origin)
-        )}`
-      )
+      .hasAttribute('href', /^https:\/\/twitter\.com\/intent\/tweet/)
       .hasClass('share')
       .hasClass('button')
       .containsText('Tweet this!');
+
+    assert.equal(
+      this.tweetParam('url'),
+      new URL('/foo/bar?baz=true#some-section', window.location.origin)
+    )
+  });
+
+  test('it supports passing @text', async function (assert) {
+    await render(
+      hbs`<ShareButton @text="Hello Twitter!">Tweet this!</ShareButton>`
+    );
+
+    assert.equal(this.tweetParam('text'), 'Hello Twitter!');
+  });
+
+  test('it supports passwing @hashtags', async function (assert) {
+    await render(
+      hbs`<ShareButton @hashtags="foo,bar,baz">Tweet this!</ShareButton>`
+    );
+
+    assert.equal(this.tweetParam('hashtags'), 'foo,bar,baz');
+  });
+
+  test('it supports passwing @via', async function (assert) {
+    await render(
+      hbs`<ShareButton @via="emberjs">Tweet this!</ShareButton>`
+    );
+
+    assert.equal(this.tweetParam('via'), 'emberjs');
+  });
+
+  test('it supports adding extra classes', async function (assert) {
+    await render(
+      hbs`<ShareButton class="extra things">Tweet this!</ShareButton>`
+    );
+
+    assert
+      .dom('a')
+      .hasClass('share')
+      .hasClass('button')
+      .hasClass('extra')
+      .hasClass('things');
+  });
+
+  test('the target, rel and href attributes cannot be overriden', async function(assert) {
+    await render(
+      hbs`<ShareButton target="_self" rel="" href="/">Not a Tweet!</ShareButton>`
+    );
+
+    assert
+      .dom('a')
+      .hasAttribute('target', '_blank')
+      .hasAttribute('rel', 'external nofollow noopener noreferrer')
+      .hasAttribute('href', /^https:\/\/twitter.com\/intent\/tweet/);
   });
 });
