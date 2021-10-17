c0b57de5bcb3dd10c057517a5acecd94805d1f53
Waihon Yew <yewwaihon@gmail.com>
2021-10-17 16:34:33 +0800

    Update rental component test with access to id

--- tests/integration/components/rental-test.js
+++ tests/integration/components/rental-test.js
@@ -15,6 +15,7 @@ module('Integration | Component | rental', function (hooks) {
     // using setProperties.
     this.setProperties({
       rental: {
+        id: 'grand-old-mansion',
         title: 'Grand Old Mansion',
         owner: 'Veruca Salt',
         city: 'San Francisco',
@@ -34,6 +35,8 @@ module('Integration | Component | rental', function (hooks) {
 
     assert.dom('article').hasClass('rental');
     assert.dom('article h3').hasText('Grand Old Mansion');
+    assert.dom('article h3 a')
+      .hasAttribute('href', '/rentals/grand-old-mansion');
     assert.dom('article .detail.owner').includesText('Veruca Salt');
     assert.dom('article .detail.type').includesText('Standalone');
     assert.dom('article .detail.location').includesText('San Francisco');
