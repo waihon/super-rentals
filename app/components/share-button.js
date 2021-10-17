c5e759ac851f3b4a569975e42cd200512d2a8373
Waihon Yew <yewwaihon@gmail.com>
2021-10-17 21:13:42 +0800

    Inject the router service to provide the current logical and relative URL

--- app/components/share-button.js
+++ app/components/share-button.js
@@ -1,10 +1,23 @@
+import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
 const TWEET_INTENT = 'https://twitter.com/intent/tweet';
 
 export default class ShareButtonComponent extends Component {
+  // Services are scoped to the app, instead of all the 
+  // JavaScript code that is running on the same page.
+  // This allows us to have multiple scripts running on 
+  // the same page without interfering with each other.
+  // Besides, services are designed to be easily swappable.
+  // Injects the router service into the component, making it
+  // available to us as this.router.
+  @service router;
+
   get currentURL() {
-    return window.location.href;
+    // currentURL provides the current "logical" and relative URL,
+    // so we would have to join it with window.location.origin
+    // to get an absolute URL that we can share.
+    return new URL(this.router.currentURL, window.location.origin);
   }
 
   get shareURL() {
