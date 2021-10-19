43a82cbe55475fddd75893f05c3377796f466cf7
Waihon Yew <yewwaihon@gmail.com>
2021-10-19 11:31:19 +0800

    Load the rental model in index and rental routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,41 +1,23 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  // Ember Data provides a store service, which we can inject into our route
+  // using the @service store declaration, making the Ember Data store
+  // available as this.store.
+  // The Ember Data store acts as a kind of intermediary between out app and the
+  // server; it does many important things, including caching the resposnes taht
+  // were fetched from the server.
+
+  @service store;
+
   // This async method is akso known as a model hook.
   // It is responsible for fetching and preparing any data that we need for our route.
   // Ember will automatically call this hook when entering a route.
   // The object returned from this hook is known as the model for the rouote.
   async model() {
-    // Use the browser's Fetch API to request JSON data from our
-    // server's API at public/api/rentals.json.
-    // The Fetch API returns a response object asynchronously.
-    let response = await fetch('/api/rentals.json');
-
-    // We knew the server sent the data using the JSON format,
-    // so we can use the json() method to parse the response data accordingly.
-    // Parsing the reponse data is also an asynchronous operation.
-    // JSON:API format returns an array nested under the "data" key
-    let { data } = await response.json();
-
-    return data.map((model) => {
-      let { id, attributes } = model;
-      let type;
-
-      // The data coming from the server is missing the type property,
-      // wich previously existed on our hard-coded model object.
-      // The type property could either be "Standaline" or "Community",
-      // depending on the type of rental property, which is required by
-      // our Rental component.
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone';
-      }
-
-      // Return id for the rental route which has a dynamic segment
-      return { id, type, ...attributes };
-    });
+    // findAll method takes the model type as an argument and fetches all records
+    // of that type from the store.
+    return this.store.findAll('rental');
   }
 }
